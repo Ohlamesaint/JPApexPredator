bc82a51bb4a15b7851fed047ec02766bda077c40
Ohlamesaint <41318666+Ohlamesaint@users.noreply.github.com>
2025-10-19 20:25:57 +0800

    init: add pr ci flow

---
+++ JPApexPredatorsTests/JPApexPredatorsTests.swift
@@ -0,0 +1,16 @@
+//
+//  JPApexPredatorsTests.swift
+//  JPApexPredatorsTests
+//
+//  Created by 陳力聖 on 2025/10/19.
+//
+
+import Testing
+
+struct JPApexPredatorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
